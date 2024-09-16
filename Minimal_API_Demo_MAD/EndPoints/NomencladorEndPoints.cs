603729cabd8d423efe5613952b0e8a002cba383a
marceloadideo <marcelo.a.dideo@outlook.com>
2024-09-16 11:21:29 -0300

    Hago el Insert duro. Ahora vamos a ver como se hace con el formulario de sqagger

--- Minimal_API_Demo_MAD/EndPoints/NomencladorEndPoints.cs
+++ Minimal_API_Demo_MAD/EndPoints/NomencladorEndPoints.cs
@@ -1,4 +1,5 @@
 ﻿using Dapper;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Minimal_API_Demo_MAD.Models;
 using System.Data.SqlClient;
@@ -16,6 +17,31 @@ public static class NomencladorEndPoints
             const string sql = "Select * from Nomencladores";
             var nomencladores = await connection.QueryAsync<Nomencladores>(sql);
             return Results.Ok(nomencladores);
+
+
+        });
+
+        builder.MapPost("nomencladores", async (IConfiguration configuration) =>
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection")!;
+            using var connection = new SqlConnection(connectionString);
+            /*
+            ExecuteNonQuery(InsertStatement, System.Data.CommandType.Text, new SqlParameter[] {
+                                    new SqlParameter("@idTa_Vaccine", Guid.NewGuid()) ,
+                                    new SqlParameter("@Code", obj.Code) ,
+                                    new SqlParameter("@Description", obj.Description) ,
+                                    new SqlParameter("@idTy_Immunization", obj.Immunization.idTa_Combo) ,
+                                    new SqlParameter("@AgeMonth", obj.AgeMonth) ,
+                                    new SqlParameter("@Notes", obj.Notes)
+
+
+
+            */
+
+            const string sql = "declare @descripcion varchar(40);INSERT INTO Nomencladores (codigo,descripcion)  VALUES  ('222222',@descripcion)";
+            var nomencladores = await connection.QueryAsync<Nomencladores>(sql);
+            return Results.Ok(nomencladores);
+
 
         });
     }
